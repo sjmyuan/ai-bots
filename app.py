4ad4d8cd46af3fbcdf6356d69666e26b05624af7
sjmyuan <shang.jiaming@163.com>
2024-10-01 22:45:59 +0800

    support multiple bot and sessions

--- app.py
+++ app.py
@@ -7,39 +7,46 @@ from yaml.loader import SafeLoader
 
 from botpage import botpage
 
-with open('./config.yml') as file:
+with open("./config.yml") as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days']
+    config["credentials"],
+    config["cookie"]["name"],
+    config["cookie"]["key"],
+    config["cookie"]["expiry_days"],
 )
 
 authenticator.login()
 
-if st.session_state['authentication_status']:
+if st.session_state["authentication_status"]:
     authenticator.logout()
+    if "bots" not in st.session_state:
+        st.session_state.bots = config["bots"]
+
+    if "bot_sessions" not in st.session_state:
+        st.session_state.bot_sessions = []
+
     with st.sidebar:
         st.subheader("新建对话")
-        for bot in ["全栈工程师", "哲学大师", "专家医生"]:
-            st.button(bot)
+        for bot in st.session_state.bots:
+            if st.button(bot["name"]):
+                botpage({"name": None, "bot_id": bot["id"], "messages": []})
 
         st.subheader("会话记录")
 
-    if "bots" not in st.session_state:
-        st.info(f"There is no bot defined.")
+        for session in st.session_state.bot_sessions:
+            if st.button(session["name"]):
+                botpage(session)
+
+    init_bot = next(b for b in st.session_state.bots)
+    if not init_bot:
+        st.info(f"There is no bot")
         st.stop()
 
-    bot = next(b for b in st.session_state.bots if b.id == bot_id)
-    if not bot:
-        st.info(f"Can not find bot by id {bot_id}")
-        st.stop()
+    botpage({"name": None, "bot_id": init_bot["id"], "messages": []})
 
-    botpage(bot)
-
-elif st.session_state['authentication_status'] is False:
-    st.error('Username/password is incorrect')
-elif st.session_state['authentication_status'] is None:
-    st.warning('Please enter your username and password')
+elif st.session_state["authentication_status"] is False:
+    st.error("Username/password is incorrect")
+elif st.session_state["authentication_status"] is None:
+    st.warning("Please enter your username and password")
--- botpage.py
+++ botpage.py
@@ -2,34 +2,39 @@ from openai import OpenAI
 import streamlit as st
 
 
-def botpage(bot):
+def botpage(session):
 
-    st.title(bot.name)
-    st.caption(bot.description)
+    bot = next(b for b in st.session_state.bots if b["id"] == session["bot_id"])
 
-    system_prompt = {'role': 'system', 'content': bot.prompt}
-    client = OpenAI(api_key=bot.api_key, base_url= bot.base_url)
+    st.title(bot["name"])
 
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+    system_prompt = {"role": "system", "content": bot["prompt"]}
 
-    for msg in st.session_state.messages:
-         if msg.bot_id == bot.id:
-            with st.chat_message(msg["role"]):
-                st.markdown(msg["content"])
+    client = OpenAI(api_key=bot["api_key"], base_url=bot["base_url"])
+
+    for msg in session["messages"]:
+        with st.chat_message(msg["role"]):
+            st.markdown(msg["content"])
 
     if prompt := st.chat_input():
-        st.session_state.messages.append({"bot_id": bot.id, "role": "user", "content": prompt})
+        session["messages"].append({"role": "user", "content": prompt})
 
         with st.chat_message("user"):
             st.markdown(prompt)
 
-
         with st.chat_message("assistant"):
             stream = client.chat.completions.create(
-                model=bot.model,
-                messages= [system_prompt] + [{"role":msg.role, "content":msg.content} for msg in st.session_state.messages if msg.bot_id == bot_id],
+                model=bot["model"],
+                messages=[system_prompt]
+                + [
+                    {"role": msg["role"], "content": msg["content"]}
+                    for msg in session["messages"]
+                ],
                 stream=True,
             )
             response = st.write_stream(stream)
-        st.session_state.messages.append({"bot_id":bot.id, "role": "assistant", "content": response})
+        session["messages"].append({"role": "assistant", "content": response})
+
+        if not session["name"]:
+            session["name"] = prompt[0:10]
+            st.session_state.bot_sessions.append(session)
