f2a27556f490bfc43b10c143a51d413d0bb46439
sjmyuan <shang.jiaming@163.com>
2025-02-05 20:20:42 +0800

    decouple bot and model

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import time
 from openai import OpenAI
 import streamlit as st
 import streamlit_authenticator as stauth
@@ -36,23 +37,50 @@ if st.session_state["authentication_status"]:
     ):
         st.session_state.bots = config["bots"]
 
+    if "models" not in st.session_state or len(st.session_state.models) != len(
+        config["models"]
+    ):
+        st.session_state.models = config["models"]
+
     if "bot_sessions" not in st.session_state:
         st.session_state.bot_sessions = []
 
     init_bot = next(b for b in st.session_state.bots)
+    init_model = next(m for m in st.session_state.models)
 
     if not init_bot:
         st.info(f"There is no bot")
         st.stop()
 
+    if not init_model:
+        st.info(f"There is no model")
+        st.stop()
+
     if "current_session" not in st.session_state:
         st.session_state.current_session = {
+            "id": int(time.time()),
             "name": None,
             "bot_id": init_bot["id"],
             "messages": [],
         }
 
+    if "current_model" not in st.session_state:
+        st.session_state.current_model = init_model
+
     with st.sidebar:
+        selected_model = st.selectbox(
+            "选择模型",
+            st.session_state.models,
+            next(
+                index
+                for index, value in enumerate(st.session_state.models)
+                if value["id"] == st.session_state.current_model["id"]
+            ),
+            lambda m: m["name"],
+        )
+
+        st.session_state.current_model = selected_model
+
         st.subheader("新建对话")
         for bot in st.session_state.bots:
             st.button(
@@ -60,6 +88,7 @@ if st.session_state["authentication_status"]:
                 on_click=set_current_session,
                 args=(
                     {
+                        "id": int(time.time()),
                         "name": None,
                         "bot_id": bot["id"],
                         "messages": [],
@@ -72,9 +101,10 @@ if st.session_state["authentication_status"]:
         for session in st.session_state.bot_sessions:
             st.button(
                 session["name"],
+                key=session["id"],
                 on_click=set_current_session,
                 args=(session,),
-                disabled=session["name"] == st.session_state.current_session["name"],
+                disabled=session["id"] == st.session_state.current_session["id"],
                 use_container_width=True,
             )
 
--- botpage.py
+++ botpage.py
@@ -6,15 +6,19 @@ def botpage():
 
     session = st.session_state.current_session
 
+    model = st.session_state.current_model
+
     bot = next(b for b in st.session_state.bots if b["id"] == session["bot_id"])
 
     name = session["name"] or bot["name"]
 
     st.title(name)
+    st.caption("Bot：" + bot["name"])
+    st.caption("模型：" + model["name"])
 
     system_prompt = {"role": "system", "content": bot["prompt"]}
 
-    client = OpenAI(api_key=bot["api_key"], base_url=bot["base_url"])
+    client = OpenAI(api_key=model["api_key"], base_url=model["base_url"])
 
     for msg in session["messages"]:
         with st.chat_message(msg["role"]):
@@ -32,7 +36,7 @@ def botpage():
 
         with st.chat_message("assistant"):
             stream = client.chat.completions.create(
-                model=bot["model"],
+                model=model["model"],
                 messages=(
                     system_prompt_list
                     + [
